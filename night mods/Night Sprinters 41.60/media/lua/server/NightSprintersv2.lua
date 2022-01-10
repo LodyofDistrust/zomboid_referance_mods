3899d1584340dd9ac9b0e37bf75f2b8b4851afa5
LodyofDistrust <97338200+LodyofDistrust@users.noreply.github.com>
2022-01-10 08:27:35 -0500

    added more variables

--- night mods/Night Sprinters 41.60/media/lua/server/NightSprintersv2.lua
+++ night mods/Night Sprinters 41.60/media/lua/server/NightSprintersv2.lua
@@ -8,18 +8,42 @@ local function ZombieChange(zombie)
 	local vMod = zombie:getModData();
 	vMod.Ticks = vMod.Ticks or 0;
 	vMod.Speed = vMod.Speed or -1;
+	
 	local speedLore = getSandboxOptions():getOptionByName("ZombieLore.Speed"):getValue();
+	local strengthLore = getSandboxOptions():getOptionByName("ZombieLore.Strength"):getValue();
+	local toughnessLore = getSandboxOptions():getOptionByName("ZombieLore.Toughness"):getValue();
+	local cognitionLore = getSandboxOptions():getOptionByName("ZombieLore.Cognition"):getValue();
+	local memoryLore = getSandboxOptions():getOptionByName("ZombieLore.Memory"):getValue();
+	local sightLore = getSandboxOptions():getOptionByName("ZombieLore.Sight"):getValue();
+	local hearingLore = getSandboxOptions():getOptionByName("ZombieLore.Hearing"):getValue();
+	local smellLore = getSandboxOptions():getOptionByName("ZombieLore.Smell"):getValue();
+	
+	--------- change "vMod.Speed ~= speedLore" to "vMod.Cognition ~= cognitionLore" if not changing speed
 	-- is update needed?             if so is it single player?             -- or maybe it's a client with zombie ownership within range
 	if vMod.Speed ~= speedLore and ((not isClient() and not isServer()) or (isClient() and not zombie:isRemoteZombie())) then
 		zombie:makeInactive(true);
 		zombie:makeInactive(false);
 		vMod.Speed = speedLore;
+		vMod.Strength = strengthLore;
+		vMod.Toughness = toughnessLore;
+		vMod.Cognition = cognitionLore;
+		vMod.Memory = memoryLore;
+		vMod.Sight = sightLore;
+		vMod.Hearing = hearingLore;
+		vMod.Smell = smellLore;
 		vMod.Ticks = 0;
 	--   Has 5 seconds past since last update?            (I know I can just use an or statement with the top if statement, but it's getting hard to read up there)
 	elseif vMod.Ticks >= updateInterval then
 		zombie:makeInactive(true);
 		zombie:makeInactive(false);
 		vMod.Speed = speedLore;
+		vMod.Strength = strengthLore;
+		vMod.Toughness = toughnessLore;
+		vMod.Cognition = cognitionLore;
+		vMod.Memory = memoryLore;
+		vMod.Sight = sightLore;
+		vMod.Hearing = hearingLore;
+		vMod.Smell = smellLore;
 		vMod.Ticks = 0;
 		--print("reupdated");
 	else
@@ -34,4 +58,3 @@ local function gameStart()
 end
 
 Events.OnGameStart.Add(gameStart);
-
